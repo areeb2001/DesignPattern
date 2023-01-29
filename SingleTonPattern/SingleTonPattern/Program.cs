b22821ef80435c1fdb6176955c7e56d61998c441
areeb2001 <68121921+areeb2001@users.noreply.github.com>
2023-01-29 14:45:31 +0500

    Add files via upload

---
+++ SingleTonPattern/SingleTonPattern/Program.cs
@@ -0,0 +1,31 @@
+﻿using SingleTonPattern;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+       
+            Singleton s0 = Singleton.getsingleton();
+            Console.WriteLine("Hash Code of obj 1 "+s0.GetHashCode());
+            Singleton.UniqueInstance = null;
+
+            Singleton s1 = Singleton.getsingleton();
+            Console.WriteLine("Hash Code of obj 2 " + s1.GetHashCode());
+            Singleton.UniqueInstance = null;
+
+            Singleton s2 = Singleton.getsingleton();
+            Console.WriteLine("Hash Code of obj 3 " + s2.GetHashCode());
+            Singleton.UniqueInstance = null;
+
+            Singleton s3 = Singleton.getsingleton();
+            Console.WriteLine("Hash Code of obj  4 " + s3.GetHashCode());
+            Singleton.UniqueInstance = null;
+            Singleton s4 = Singleton.getsingleton();
+            Console.WriteLine("Hash Code of obj  5  " + s4.GetHashCode());
+            Singleton.UniqueInstance = null;
+            Singleton s5 = Singleton.getsingleton();
+        Console.ReadKey();
+
+
+    }
+}
---
+++ SingleTonPattern/SingleTonPattern/Singleton.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingleTonPattern
+{
+    public class Singleton
+    {
+        public static int count;
+        private Singleton()
+        {
+            count++;
+        }
+
+        private static Singleton uniqueinstance = null;
+
+
+        public static Singleton UniqueInstance { set { uniqueinstance = value; } }
+
+
+        public static Singleton getsingleton()
+        {
+            if (uniqueinstance == null)
+            {
+                if (count < 5)
+                {
+                    uniqueinstance = new Singleton();
+                    return uniqueinstance;
+                }
+                else
+                {
+                    Console.WriteLine("Soory You cannot make More Object");
+                }
+
+            }
+                return uniqueinstance;
+            }
+      
+    }
+}
