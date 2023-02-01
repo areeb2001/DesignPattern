93bde8cad9570389989193876cad5f3c1e5cc991
areeb2001 <68121921+areeb2001@users.noreply.github.com>
2023-02-01 13:01:41 +0500

    Add files via upload

---
+++ abstractFactory/abstractFactory/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace abstractFactory
+{
+    class Program
+    {
+        public abstract class patient
+        {
+            public abstract void dignose();
+        }
+        public class patient_emergency : patient
+        {
+            public override void dignose()
+            {
+
+            }
+        }
+        public class relationship_patient : patient
+        {
+            public override void dignose()
+            {
+
+            }
+        }
+        public class designPattern_patient : patient
+        {
+            public override void dignose()
+            {
+                
+            }
+        }
+        public class hospital
+        {
+            public patient getPatient(string patientType)
+            {
+                if (patientType == "emergency")
+                {
+                    return new patient_emergency();
+                }
+                else if(patientType=="relationship")
+                {
+                    return new relationship_patient();
+                }
+                else if (patientType=="designPattern")
+                {
+                    return new designPattern_patient();
+                }
+                return null;
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ adaptor/adaptor/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace adaptor
+{
+    public class SquarePeg : ISquarepeg
+    {
+        public void Insert(string msg)
+        {
+            Console.WriteLine("SquarePeg Insert method:  " + msg);
+        }
+    }
+    public interface IRoundPeg
+    {
+        void InsertIntoHole(string msg);
+    }
+    public interface ISquarepeg
+    {
+        void Insert(string msg);
+    }
+    public class RoundPeg : IRoundPeg
+    {
+        public void InsertIntoHole(string msg)
+        {
+            Console.WriteLine("Roundpeg insertIntohole method " + msg);
+        }
+    }
+    public class PegAdapter : ISquarepeg, IRoundPeg
+    {
+        private RoundPeg roundPeg;
+        private SquarePeg squarePeg;
+        public PegAdapter(RoundPeg round)
+        {
+            roundPeg = round;
+        }
+        public PegAdapter(SquarePeg square)
+        {
+            squarePeg = square;
+        }
+        public void Insert(string msg)
+        {
+            roundPeg.InsertIntoHole(msg);
+        }
+        public void InsertIntoHole(string msg)
+        {
+            squarePeg.Insert(msg);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RoundPeg roundPeg = new RoundPeg();
+            SquarePeg squarePeg = new SquarePeg();
+
+            squarePeg.Insert("Insert Into SquarePeg");
+
+            ISquarepeg roundToSquare = new PegAdapter(roundPeg);
+            roundToSquare.Insert("Inserting round peg ");
+
+            roundPeg.InsertIntoHole("Inserting round peg ");
+
+            IRoundPeg squareToRound = new PegAdapter(squarePeg);
+            squareToRound.InsertIntoHole("Inserting square peg ");
+        }
+    }
+}
