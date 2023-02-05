e3ada3b4bff154a96827f89d7f4c0c53feae8cda
areeb2001 <68121921+areeb2001@users.noreply.github.com>
2023-02-05 12:14:02 +0500

    Add files via upload

---
+++ ProxyDesignPattern/ProxyDesignPattern/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace ProxyDesignPattern
+{
+    public interface INest
+    {
+        void Access(string name);
+    }
+    public class RealNest : INest
+    {
+        public void Access(string name)
+        {
+            Console.WriteLine($"{name} has access to the nest");
+        }
+    }
+    public class SecureNestProxy : INest
+    {
+        private readonly INest nest;
+
+        public SecureNestProxy(INest nest)
+        {
+            this.nest = nest;
+        }
+
+        public void Access(string name)
+        {
+            if (name == "TRex" || name == "Gigantosaurus")
+            {
+                Console.WriteLine($"{name} is not allowed to access the nest.");
+            }
+            else
+            {
+                this.nest.Access(name);
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var secureNestProxy = new SecureNestProxy(new RealNest());
+
+            secureNestProxy.Access("Stegosaurus");
+            secureNestProxy.Access("TRex");
+
+        }
+    }
+}
